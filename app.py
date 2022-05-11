c370106820c608a111411949694a05159b2bae40
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2022-05-11 15:41:22 +0800

    modify config

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from imutils.perspective import four_point_transform
 from skimage.filters import threshold_local
 import numpy as np
-import argparse
 import cv2
 import imutils
 import os 
